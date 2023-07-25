68c5fcc93b2b94ff35b853c601f552cb6846a524
Asrar-Ahammad <asrarahammadshaik@gmail.com>
2023-07-25 06:13:46 +0530

    Merge two sorted Arrays

--- competitive_coding/merge_sorted_array.py
+++ competitive_coding/merge_sorted_array.py
@@ -1,11 +1,17 @@
 def mergeSortedArray(nums1, m, nums2, n):
-    for i in nums1:
-        if nums1 == 0:
-            nums1.pop(nums1.index(i))
-    return nums1
+    i = m-n
+    j = 0
+    while(i<m):
+        nums1[i] = nums2[j]
+        i+=1
+        j+=1
+    nums1 = sorted(nums1)
+    print(nums1)
+    # for i in range(m - n):
+    #     print(nums1[i])
 
-nums1 = [1,2,3,4,0,0,0,0]
+nums1 = [1,2,3,0,0,0]
 m = len(nums1)
 nums2 = [2,5,6]
 n = len(nums2)
-print(mergeSortedArray(nums1,m,nums2,n))
+mergeSortedArray(nums1, m, nums2, n)
